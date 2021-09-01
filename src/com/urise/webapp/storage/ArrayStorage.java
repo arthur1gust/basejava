e6036b9fa1837ef9de1582a4051358f14411112c
Artur <gustaytsev@mail.ru>
2021-09-01 15:23:00 +0700

    create project structure

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,41 +1,45 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  *
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
     }
 
-    void save(Resume r) {
-        if (get(r.uuid) == null) {
+    public void save(Resume r) {
+        if (get(r.getUuid()) == null) {
             storage[size] = r;
             size++;
         } else {
-            System.out.println(r.uuid + " -already exist, please another uuid");
+            System.out.println(r.getUuid() + " -already exist, please another uuid");
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for(int i = 0; i < size; i++) {
-            if(uuid.equals(storage[i].uuid)) {
+            if(uuid.equals(storage[i].getUuid())) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int i = 0;
         boolean checkUuid = false;
 
         while (i < size) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 checkUuid = true;
             }
 
@@ -51,7 +55,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] allResume = new Resume[size];
         for (int i = 0; i < size; i++) {
             allResume[i] = storage[i];
@@ -59,7 +63,7 @@ public class ArrayStorage {
         return allResume;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
