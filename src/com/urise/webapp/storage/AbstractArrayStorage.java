b84afb9c241761d52e8f19cb2eabb9117e30b25b
Artur <gustaytsev@mail.ru>
2021-10-11 17:31:06 +0700

    дз-3_Abstract

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -7,16 +7,61 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public abstract class AbstractArrayStorage implements Storage{
+public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10000;
 
-    public Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    @Override
     public int size() {
         return size;
     }
 
+    @Override
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    @Override
+    public void save(Resume r) {
+        if (size >= STORAGE_LIMIT) {
+            System.out.println("Превышено максимально возможное количество резюме - " + size);
+        } else if (findIndex(r.getUuid()) == -1) {
+            storage[size] = r;
+            size++;
+            System.out.println(r.getUuid() + " - резюме сохранено");
+        } else {
+            System.out.println(r.getUuid() + " - данный uuid уже существует, пожалуйста, введите новый");
+        }
+    }
+
+    @Override
+    public void update(Resume r) {
+        int updateResume = findIndex(r.getUuid());
+        if (updateResume == -1) {
+            System.out.println(r.getUuid() + " - резюме не найдено");
+        } else {
+            storage[updateResume] = r;
+            System.out.println(r.getUuid() + " - резюме обновлено");
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int deleteResume = findIndex(uuid);
+        if (deleteResume == -1) {
+            System.out.println(uuid + " - резюме не найдено");
+        } else {
+            storage[deleteResume] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+            System.out.println(uuid + " - резюме удалено");
+        }
+    }
+
+    @Override
     public Resume get(String uuid) {
         int getResume = findIndex(uuid);
         if (getResume == -1) {
@@ -24,6 +69,11 @@ public abstract class AbstractArrayStorage implements Storage{
             return null;
         }
         return storage[findIndex(uuid)];
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     protected abstract int findIndex(String uuid);
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -13,56 +13,11 @@ public class ArrayStorage extends AbstractArrayStorage {
     private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void save(Resume r) {
-        if (size >= STORAGE_LIMIT) {
-            System.out.println("Превышено максимально возможное количество резюме - " + size);
-        } else if (findIndex(r.getUuid()) == -1) {
-            storage[size] = r;
-            size++;
-            System.out.println(r.getUuid() + " - резюме сохранено");
-        } else {
-            System.out.println(r.getUuid() + " - данный uuid уже существует, пожалуйста, введите новый");
-        }
-    }
-
-    public void update(Resume r) {
-        int updateResume = findIndex(r.getUuid());
-        if (updateResume == -1) {
-            System.out.println(r.getUuid() + " - резюме не найдено");
-        } else {
-            storage[updateResume] = r;
-            System.out.println(r.getUuid() + " - резюме обновлено");
-        }
-    }
-
-    public void delete(String uuid) {
-        int deleteResume = findIndex(uuid);
-        if (deleteResume == -1) {
-            System.out.println(uuid + " - резюме не найдено");
-        } else {
-            storage[deleteResume] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-            System.out.println(uuid + " - резюме удалено");
-        }
-    }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
 
-    public int size() {
-        return size;
-    }
-
+    @Override
     protected int findIndex(String uuid) {
         int index = -1;
         for (int i = 0; i < size; i++) {
